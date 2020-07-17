47ee78b1ae627f19449f1e7390d411b7889b79a2
reiwa22 <hermesreiwa@yahoo.co.jp>
2020-07-17 09:02:50 +0900

    커밋 jm

--- TeamProject/src/main/java/com/kh/team/jm/JmReviewDao.java
+++ TeamProject/src/main/java/com/kh/team/jm/JmReviewDao.java
@@ -7,7 +7,7 @@ import com.kh.team.domain.JmReviewVo;
 public interface JmReviewDao {
 
 	//리뷰 게시판 목록 조회
-	public List<JmReviewVo> selectReviewList() throws Exception;
+	public List<JmReviewVo> selectReviewList() throws Exception; 
 	
 	
 	
